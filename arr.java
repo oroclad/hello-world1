4f2f18788e43f57072d6ac033005568103bf94e3
oroclad <876012908@qq.com>
2018-05-12 15:55:36 +0800

    xxx

---
+++ arr.java
@@ -0,0 +1,11 @@
+//length代表的是数组的长度（或者说是元素的个数）。
+//是数组的一个属性
+//length是int型
+//数组最小索引是0，最大索引是length
+public class arr {
+   public static void main(String[] args) {
+	   //数据类型 [] 变量名 = new 数据类型 [元素的个数]
+	   int [] arr = new int [117];
+	   System.out.println(arr.length);
+   }
+}
